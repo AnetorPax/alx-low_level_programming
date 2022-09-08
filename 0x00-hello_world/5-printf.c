6f2fc549d2d8e27037ac22b2c3494c95337a4fce
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-08 21:58:21 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-int main(void) 
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
 {
-	/**
-    	* using printf to output a string
-    	*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return 0;
+	return (0);
 }
