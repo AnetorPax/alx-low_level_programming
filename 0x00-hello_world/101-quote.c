975c9a4cd2832a71f60a4b219c4589b06a2707ac
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-09 04:27:36 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,10 @@ int main(void)
 int s = strlen("and that piece of art is useful\" - DoraKorpar, 2015-10-19\n"
 );
 
-write(2,
+write(1,
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 
+
 return (1);
+
 }
