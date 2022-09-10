6f35c7555562fc99753401c02e687ad52f9c7e9f
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-10 04:58:11 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,31 +8,34 @@
  * Return: Your text
  */
 
-	int main (void)
-  {
-    int n;
-    /**
-    *initialize random number
-    *
-    */
-  
-    srand ( time(0) );
-		  /* Generate a random number: */
+int main (void)
+	{
+		int n;
+		/**
+		 * *initialize random number
+		 * *
+		 *
+		 * */
 
-    n = rand() - RAND_MAX / 2;
+		srand ( time(0) );
 
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else 
-    {
-        printf("%d is negative\n", n);
-    }
-    return 0;
-  }
+		/* Generate a random number: */
+
+		n = rand() - RAND_MAX / 2;
+
+		if (n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+
+		else if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+		else 
+		{
+			printf("%d is negative\n", n);
+		}
+		return 0;
+	}
 
