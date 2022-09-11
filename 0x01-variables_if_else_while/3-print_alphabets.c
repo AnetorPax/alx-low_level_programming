82c2948a70a18503a5d3922a11fcd52dbb8d3240
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-11 16:38:34 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+ * main - Prints alphbeths in lower case and upper case
+ * Return: Always (successful)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
