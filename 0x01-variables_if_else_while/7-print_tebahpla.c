11d9a3a4b6c20e4dd5ef86a01af07fd39efeef37
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-11 18:43:40 +0100

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Prints lower case alphabeths in reverse
+ *
+ * Return: Always(successful)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	
+	return (0);
+
+}
