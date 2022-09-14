be5b1cc93e718497c46657b2f862122703b86c94
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-13 18:48:19 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - prints checked lowercase character
+ * @c: integer 1 or 0
+ * Return: Always(successful)
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
