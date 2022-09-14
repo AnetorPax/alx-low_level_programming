63f3a291599e2d4882de04d1a33cf97d3ef55d1b
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-14 03:22:01 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(d + '0');
 				_putchar(u + '0');
 			}
-			else 
+			else
 			{
 				if (y != 0)
 				{
@@ -34,8 +34,8 @@ void times_table(void)
 				_putchar(z + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
 
