8b82f765243d49cc2b3a4560ccca87aea52ef027
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-14 12:41:23 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,24 +3,29 @@
 /**
  * print_to_98 - Print from n to 98
  * @n: integer
- * Return: From n to 98
+ * Return: The function prints From n to 98
  */
 void print_to_98(int n)
 {
 	if (n < 98)
 	{
 		for (; n <= 98; n++)
+
 		{
 			printf("%d", n);
+			
 			if (n != 98)
 			{
 				printf(",");
 			}
 		}
 	}
+
 	else
+	
 	{
 		for (; n >= 98; n--)
+
 		{
 			printf("%d", n);
 			if (n != 98)
