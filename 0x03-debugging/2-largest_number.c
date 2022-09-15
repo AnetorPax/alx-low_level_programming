1cb97058a9e98d0d6aa6524c1293cd939af60566
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-16 00:04:08 +0100

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,20 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else 
 	{
 		largest = b;
 	}
-	else
+
+	if (c > largest)
 	{
 		largest = c;
 	}
 
 	return (largest);
+
 }
