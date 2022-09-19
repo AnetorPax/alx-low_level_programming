e0964ca6b6993156189ca4e7192e220aa7980ce6
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-19 22:33:55 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: the steing to be printed
+ * Return: Always successful
+ */
+
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i = 0;
+	}
+
+	k = i;
+	for(j = k - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\0');
+}
+
