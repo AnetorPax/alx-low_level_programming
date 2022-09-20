02aba842e53bbfa7d9ad09a7d50f7af82bf9b475
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-20 05:14:58 +0100

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 	int i, j, k;
 
 	i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -22,7 +22,7 @@ void print_rev(char *s)
 	{
 		_putchar(s[j]);
 	}
-	
+
 	_putchar('\n');
 }
 
