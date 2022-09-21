83567aa3b7e9524bb912e207bf80add90f037a24
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-21 19:32:53 +0100

    sed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
  * @src: source string
  * @dest: destination string
  * @n: number of bytes to be copied
+ * Return: the resulting string a dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
