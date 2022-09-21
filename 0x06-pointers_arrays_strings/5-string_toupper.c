59711d4e22b8adb12b6dadb93825494661e662c8
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-21 22:58:16 +0100

    sed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * *string_toupper - changes all lowercase letters 
+ * *string_toupper - changes all lowercase letters
  * of a string to uppercase.
  * @a: string to modify
  * Return: Resulting string
