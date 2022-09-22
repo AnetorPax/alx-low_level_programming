1e2ce1a34e33307d3752ee6aa8d56162482daa08
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-22 16:50:25 +0100

    gh

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,20 +9,19 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char n[] = "AZCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char m[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char a[] = "AZCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; n[j] != '\0'; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == n[j])
+			if (s[i] == a[j])
 			{
-				s[i] = m[j];
+				s[i] = b[j];
 				break;
 			}
 		}
 	}
-	
 	return (s);
 }
