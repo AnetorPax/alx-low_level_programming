df50371175f98a84f6c19e838f609f08e32b4c5b
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-22 02:27:37 +0100

    fuh

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,9 @@
 #include"main.h"
+
 /**
  * cap_string - capitalize every element of a string
  * @s: string to modify
- * Return: resulting string
+ * Return: the resulting string
  */
 
 char *cap_string(char *s)
@@ -15,15 +16,15 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] = 32;
+			s[i] -= 32;
 
-		for (j = 0; j < 13; j++)
+		for (j = 0; j <= 13; j++)
 		{
 			if (s[i] == spe[j])
 			{
 				if (s[i + 1] >= 'a' && s[i +1] <= 'z')
 				{
-					s[i + 1] = 32;
+					s[i + 1] -= 32;
 				}
 			}
 		}
