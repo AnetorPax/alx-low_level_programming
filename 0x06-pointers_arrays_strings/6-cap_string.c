dc112bf68006aed58ad32b929b1a272b78907811
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-22 02:41:57 +0100

    yy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
 /**
  * cap_string - capitalize every element of a string
  * @s: string to modify
+ *
  * Return: the resulting string
  */
 
@@ -18,11 +19,11 @@ char *cap_string(char *s)
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 
-		for (j = 0; j <= 13; j++)
+		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == spe[j])
 			{
-				if (s[i + 1] >= 'a' && s[i +1] <= 'z')
+				if (s[i + 1] >= 'a'	&& s[i +1] <= 'z')
 				{
 					s[i + 1] -= 32;
 				}
