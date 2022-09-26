3b711291ecdcb17f5cc7ec8ae5977e9c279f4b2c
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-09-27 00:11:02 +0100

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies to a memory area
+ * @n: the number of byte to be copied
+ * @dest: destination memory area 
+ * @src: source memory area
+ * Return: Pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
