2d93ebf889f4f98475f0662fc79b24e82b170906
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-01 22:13:24 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,8 @@
  * @argv: array of arguements 
  * Return: Always (successful)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-
-	printf("\n");
+	printf("%d\n", argc - 1);
+	return (0);
 }
