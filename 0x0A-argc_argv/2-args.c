91be9b1c5dac947f216ee98d6a00f3d175e5ec87
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-01 22:23:42 +0100

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints all arguements it recieves
+ * @argc: number of arguements 
+ * @argv: array of arguememts
+ * Return: Always(successful)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
