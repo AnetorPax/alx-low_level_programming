407882794a8cdc0b6a5ba9c20f939338a6c7f0a5
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-02 10:57:53 +0100

    sed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,7 +4,7 @@
 /**
  * main - prints the minimum number of coins
  * to make change for an amount of money
- * @argc: nuumber of arguements 
+ * @argc: nuumber of arguements
  * @argv: array of arguements
  * Return: 0 (success), 1 (Error)
  */
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
