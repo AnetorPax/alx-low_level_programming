bcdd567f8d0bec3899dabb05c00efb14bdd74a48
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-03 16:29:58 +0100

    new

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * oh_prime - prime number 
- * @i: input number 
+ * oh_prime - prime number
+ * @i: input number
  * @j: iterator
  * Return: 1 if prime else 0
  */
@@ -19,17 +19,17 @@ int oh_prime(unsigned int i, unsigned int j)
 }
 
 /**
- * is_prime_number - prints a prime number 
+ * is_prime_number - prints a prime number
  * @n: input number
  * Return: 1 if input is a prime number else 0
  */
 int is_prime_number(int n)
 {
 	if (n == 0)
-		return 0;
+		return (0);
 	if (n < 0)
-		return 0;
+		return (0);
 	if (n == 1)
-		return 0;
+		return (0);
 	return (oh_prime(n, 2));
 }
