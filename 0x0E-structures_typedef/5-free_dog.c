f2f2b83db3b7f680f3ad4f7325beb6012a99b831
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-12 03:56:29 +0100

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,14 @@
 #include  "dog.h"
 /**
+ * free_dog - frees memory allocates for the struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
