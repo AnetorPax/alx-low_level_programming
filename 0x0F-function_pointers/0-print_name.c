d16947122cad2e3ffe7af6eca1fd9c96cc16deff
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-13 22:35:03 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
- * print_name - function that prints a name
- * @name: name to print
+ * print_name - prints a name
+ * @name: name to be printed
  * @f: pointer to the function
- * Return: Name
+ * Return: name
  */
 void print_name(char *name, void (*f)(char *))
 {
