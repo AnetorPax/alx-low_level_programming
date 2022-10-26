3b7b9ec8675d61db5a0a8efc07aefafd693413bd
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-26 03:54:13 +0100

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: pointing to the head 
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *headcp = head, *next;
+
+	while (headcp != NULL)
+	{
+		next = headcp->next;
+		free(headcp);
+		headcp = next;
+	}
+	head = NULL;
+}
