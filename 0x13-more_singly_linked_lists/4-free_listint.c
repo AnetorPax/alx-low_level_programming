fda1928b55b013332b0a9eb45793011d299f2de1
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-26 17:51:55 +0100

    sed

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * free_listint - function that frees a listint_t list
- * @head: pointing to the head 
+ * @head: pointing to the head
  */
 
 void free_listint(listint_t *head)
