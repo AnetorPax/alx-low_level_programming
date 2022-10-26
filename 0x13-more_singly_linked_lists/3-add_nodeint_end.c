8a6fa2871b2449e0d1f39fbe23ed7db412effe1a
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-26 17:56:11 +0100

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,18 +15,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	
 	new->n = n;
 	new->next = NULL;
-	
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-	
 	while (headcp->next != NULL)
-	headcp = headcp->next;
+		headcp = headcp->next;
 	headcp->next = new;
 	return (new);
 }
