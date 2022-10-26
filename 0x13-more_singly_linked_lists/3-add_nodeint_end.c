85906cc64f58ea6c97767800a288053e626ae1c1
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-26 03:43:50 +0100

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,12 +10,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *headcp = *head;
 
 	new = malloc(sizeof(listint_t));
+	
 	if (new == NULL)
 	{
 		return (NULL);
 	}
+	
 	new->n = n;
 	new->next = NULL;
+	
 	if (*head == NULL)
 	{
 		*head = new;
