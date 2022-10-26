da070bd8f81ffb97f1d26169dc82bf72e85b4836
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-26 17:46:20 +0100

    sed

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,13 +3,12 @@
  * pop_listint - deletes the head node of a listint_t
  * linked list, and returns the head node’s data (n).
  * @head: pointer to the head
- * Return: returns the head node’s data (n)
+ * Return: returns the head node’s data
  */
 int pop_listint(listint_t **head)
 {
 	int value;
 	listint_t *headcp = *head, *next;
-	
 
 	if (headcp == NULL)
 		return (0);
