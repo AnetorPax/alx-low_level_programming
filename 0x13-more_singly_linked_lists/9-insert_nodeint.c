a287520935ddb085e4004ee3a7ae9b52fb5e95de
Peace Ogedegbe <peaceanetorogedegbe@gmail.com>
2022-10-27 06:38:25 +0100

    sed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	headcp = *head;
-	for (node = 0; (node < idx) && headcp; node ++)
+	for (node = 0; (node < idx) && headcp; node++)
 	{
 		prev = headcp;
 		headcp = headcp->next;
