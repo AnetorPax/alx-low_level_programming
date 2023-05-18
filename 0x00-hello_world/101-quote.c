19c58b6cf40741c105d6b60ae4738b67115967a0
AnetorPax <anetorpax@gmail.com>
2023-05-19 00:00:45 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program prints a string followed by
+ * a new line to the standard error
+ *
+ * Return: 1 if successful.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
