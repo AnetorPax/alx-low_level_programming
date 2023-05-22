0364e113d4c6cd95efa862b8038b752f453cea1b
AnetorPax <anetorpax@gmail.com>
2023-05-22 06:30:44 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of single digit numbers
+ * seperatedbby a comma followed by space
+ * in ascending order.
+ * Return: 0 Always Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
