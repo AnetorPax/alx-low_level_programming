02ee78e3119fdd92cadcfccdc6de6bc8c84e2564
AnetorPax <anetorpax@gmail.com>
2023-05-28 11:01:11 +0100

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * print_fibonacci_sequence - Prints the fibonacci sequence of n
+ * @n: The parameter to be examined
+ */
+void print_fibonacci_sequence(int n)
+{
+	int x;
+	unsigned long fib1;
+	unsigned long fib2;
+	unsigned long fib;
+
+	x = 2;
+	fib1 = 1;
+	fib2 = 2;
+	fib = 0;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	while (x < n)
+	{
+		fib = fib1 + fib2;
+		printf(", %lu", fib);
+
+		fib1 = fib2;
+		fib2 = fib;
+		x++;
+	}
+
+	printf("\n");
+}
+
+/**
+ * main - prints fibonacci sequence using print_fibonacci_sequence function.
+ * Return: 0 Always successful
+ */
+int main(void)
+{
+	int n;
+
+	n = 98;
+	print_fibonacci_sequence(n);
+	return (0);
+}
