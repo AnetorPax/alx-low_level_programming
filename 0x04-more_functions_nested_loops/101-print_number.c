6e3229dcf29187f3aff5b5c470434761d47aab97
AnetorPax <anetorpax@gmail.com>
2023-05-29 10:22:37 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,40 +6,15 @@
  */
 void print_number(int n)
 {
-	int num_digits, temp, divisor, i;
+	unsigned int x;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
+	x = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		x = -n;
 	}
-
-	num_digits = 0;
-	temp = n;
-
-	while (temp != 0)
-	{
-		temp /= 10;
-		num_digits++;
-	}
-
-	divisor = 1;
-	for (i = 1; i < num_digits; i++)
-	{
-		divisor *= 10;
-	}
-
-	while (divisor != 0)
-	{
-		int digit = n / divisor;
-		_putchar(digit + '0');
-		n %= divisor;
-		divisor /= 10;
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
