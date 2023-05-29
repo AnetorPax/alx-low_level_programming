0b1adc47a31c5989dbb67abfe4c7e7f075eec243
AnetorPax <anetorpax@gmail.com>
2023-05-29 23:18:47 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to be printed
+ */
+void print_rev(char *s)
+{
+	/* Declaration of a new varaibles*/
+	int len;
+	int i;
+
+
+        /* Takes care of a NULL string */
+	if ( s == NULL)
+	{
+		return;
+	}
+
+	/* Find the length of the string*/
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	/* The loop prints the string in reverse*/
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
