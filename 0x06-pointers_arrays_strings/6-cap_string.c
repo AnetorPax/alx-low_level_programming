3966aa3595081eda75d8b505283830ecc70d0cdd
AnetorPax <anetorpax@gmail.com>
2023-06-01 16:41:20 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - capatilizes all words of a string
+ * @str: String to be examined
+ * Return: str.
+ */
+char *cap_string(char *str)
+{
+	char *p = str; /*declaring and intializing new pointer variable*/
+	int cap = 1; /*Capatalize flag to set next character to capitalize*/
+
+
+	/*Loop to test and capitalize each character in the string*/
+	while (*p != '\0')
+	{
+		if (cap && *p >= 'a' && *p <= 'z')
+		{
+			*p = *p - ('a' - 'A'); /* Capitalize the current character*/
+		}
+
+		/* To test if current character is a seperator*/
+		if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ',' || *p == ';' ||
+		     *p == '.' || *p == '!' || *p == '?' || *p == '"' || *p == '(' ||
+		     *p == ')' || *p == '{' || *p == '}')
+		{
+			cap = 1; /*setting capitalize flag*/
+		}
+		else
+		{
+			cap = 0; /* Reset the capitalize flag */
+		}
+
+		p++;
+	}
+
+	return (str);
+}
