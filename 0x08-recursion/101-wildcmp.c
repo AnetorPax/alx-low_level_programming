f37969c58d9ef6b2a44f989dd911b556a1ac53ac
AnetorPax <anetorpax@gmail.com>
2023-06-09 05:06:17 +0100

    further corrections

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * wildcmp_helper - helps for recursion
+ * wildcmp - compares two strings with  recursion
  * @s1: the first string
  * @s2: the second string
  * Return: 0
  */
-int wildcmp_helper(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	/* If both strings are empty, they are considered identical*/
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
 	/* If the current characters match or s2 has a wildcard '*' */
-	if (*s1 == *s2)
-		return (wildcmp_helper(s1 + 1, s2 + 1));
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
 
-	if (*s2 == '*')
-		return (wildcmp_helper(s1, s2 + 1) || wildcmp_helper(s1 + 1, s2));
-
-	if (*s1 == '\0' && *s2 != '\0' && *s2 != '*')
-		return (0);
-
+		if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2)
+		    || wildcmp(s1, s2 + 1))
+			return (1);
+	}
 
 	/* If none of the conditions above are met, the strings are not identical */
 	return (0);
@@ -31,8 +31,9 @@ int wildcmp_helper(char *s1, char *s2)
  * @s1: first string
  * @s2: second string
  * Return: if both string are identical and 0 if otherwise.
- */
+
 int wildcmp(char *s1, char *s2)
 {
 	return (wildcmp_helper(s1, s2));
 }
+*/
