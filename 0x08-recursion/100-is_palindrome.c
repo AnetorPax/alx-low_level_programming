4cc8130c7380681c745442ae26f880399dfc8844
AnetorPax <anetorpax@gmail.com>
2023-06-09 04:06:10 +0100

    replacing loop with recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,28 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 /**
+ * is_palindrome_helper(char *s, int start, int end)
+ * @s: sting to be examined,
+ * @start: beginning of our string
+ * @end: end of our string
+ * Return: 0
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+/**
  * is_palindrome - checks if a string is palindrome
  * @s: string to be examined
  * Returns: 1 if string is pallidrome and 0 if not
  */
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
-	int i = 0;
-	int j = len - 1;
+	int end = 0;
 
+	if (s[0] == '\0')
+		return (1);
 
+	while (s[end + 1] != '\0')
+		end++;
 
-	if (s == NULL)
-		return (0);
-
-	while (i < j)
-	{
-		if (s[i] != s[j])
-			return (0);
-		i++;
-		j--;
-	}
-
-	return (1);
+	return (is_palindrome_helper(s, 0, end));
 }
