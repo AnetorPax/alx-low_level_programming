f62f151d4e75b4a30f2ee646afa8d35c013b9f01
AnetorPax <anetorpax@gmail.com>
2023-06-15 21:14:54 +0100

    trying to clear check 8 error

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,10 +11,7 @@ void *malloc_checked(unsigned int b)
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		printf("Error: Failure, Exiting... \n");
 		exit(98);
-	}
-
+	
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - It allocates memory for an array, using malloc
+ * @nmemb: the element in the array
+ * @size: the size of each element
+ * Return: NULL if nmemb or size is zero and if _callocs fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
