d0b3218f163560c4fadd16b5b9f70dd12a9afb27
AnetorPax <anetorpax@gmail.com>
2023-06-15 20:08:16 +0100

    testing if memeory was successfully allocated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - Allocates memory using malloc and exit with 98 if failure
+ * @b: the parameter to which the memory is allocated for
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		printf("Unsuccessful: Failure, Exiting... \n");
+		exit (98);
+	}
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+The directory houses files that are a test of our understanding of memory
+allocation in c programming language. with further experience on realloc, calloc
+and exit functions.
+The files are divided into
++ Mandatory task
++ Advance task.
