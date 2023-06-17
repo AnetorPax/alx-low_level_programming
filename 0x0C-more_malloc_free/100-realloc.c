a2d91e8ece4cf4fcf8358c0e8d96798775105f90
AnetorPax <anetorpax@gmail.com>
2023-06-17 14:00:59 +0100

    reallocation of memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,48 +3,49 @@
 #include <string.h>
 #include <stdlib.h>
 /**
-* _realloc - reallocates a memory block using malloc and free
-* @ptr: pointer to ne examined
-* @old_size: number of byte previously reserved for ptr
-* @new_size: new memory to be allocated.
-* Return: pointer to reallocated memory block or NULL on failure.
-*/
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to ne examined
+ * @old_size: number of byte previously reserved for ptr
+ * @new_size: new memory to be allocated.
+ * Return: pointer to reallocated memory block or NULL on failure.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 	unsigned int i;
 	char *source, *destination;
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
-
 	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
-		return (NULL);
-
 	source = (char *)ptr;
 	destination = (char *)new_ptr;
-
-	for (i = 0; i < old_size && i < new_size; i++)
-		destination[i] = source[i];
 
 	if (new_size > old_size)
 	{
 		for (; i < new_size; i++)
 			destination[i] = 0;
 	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < old_size && i < new_size; i++)
+		destination[i] = source[i];
+
 
 	free(ptr);
 
