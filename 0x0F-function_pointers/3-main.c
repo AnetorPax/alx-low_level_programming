d0dc168a421154477f41030565a6156fb6740b34
AnetorPax <anetorpax@gmail.com>
2023-06-21 22:30:16 +0100

    creating a program that selects the proper operator for arithmetic operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+/**
+ * main - performs simple arithmetic operations
+ * @argc: argument counter
+ * @argv: Argument vector
+ * Return: 0 Always Success
+ * Author: AnetorPAx
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char op;
+	int (*funct)(int, int);
+
+	funct = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	op = *argv[2];
+	arg2 = atoi(argv[3]);
+
+	if (!funct)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((op == '/' || '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = funct(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integer numbers
+ * @a: first interger
+ * @b: second integer
+ * Return: Interger varaible Sum
+ */
+int op_add(int a, int b)
+{
+	int Sum;
+
+	Sum = a + b;
+	return (Sum);
+}
+/**
+ * op_sub - Outputs the difference between two integers
+ * @a: first interger
+ * @b: second integer
+ * Return: Difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	int diff;
+
+	diff = a - b;
+	return (diff);
+}
+/**
+ * op_mul - Multiplies two integers
+ * @a: first interger
+ * @b: second integer
+ * Return: product of both integers
+ */
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a*b;
+	return (mul);
+}
+/**
+ * op_div - Outputs the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: Division of a by b
+ */
+int op_div(int a, int b)
+{
+	int div;
+
+	div = a / b;
+	return (div);
+}
+/**
+ * op_mod - outputs the remainder of a divided by b
+ * @a: first parameter
+ * @b: second parameter
+ * Return: remainder value
+ */
+int op_mod(int a, int b)
+{
+	int mod;
+
+	mod = a % b;
+	return (mod);
+}
