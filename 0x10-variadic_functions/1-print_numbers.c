ba67533cb460ca1c6df43b38396eca2445dc8e7b
AnetorPax <anetorpax@gmail.com>
2023-06-22 12:58:57 +0100

    error alternating between checker 4 and 9

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
-	if (separator == NULL)
-		return;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
+		else
+			return;
 	}
 	va_end(valist);
 
