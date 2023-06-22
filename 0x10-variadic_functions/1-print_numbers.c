fb648b1ae69799216eba0315ae5c1d62ee39fa4d
AnetorPax <anetorpax@gmail.com>
2023-06-22 12:56:27 +0100

    Checker 9 cleared, now trying to clear checker 4

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
-	/*if (separator == NULL)
-	  return;*/
+	if (separator == NULL)
+		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
