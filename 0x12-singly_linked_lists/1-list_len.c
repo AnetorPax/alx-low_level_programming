2785e2de2a29d70c4eac9442ff2048a025b5395c
AnetorPax <anetorpax@gmail.com>
2023-07-03 22:03:01 +0100

    Singly list task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a list_t list.
+ * @h: The head of the list.
+ *
+ * Return: The number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return count;
+}
