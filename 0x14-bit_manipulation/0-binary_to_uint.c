5805a7a3703704df379e7ac3752cfaabd6512b8b
AnetorPax <anetorpax@gmail.com>
2023-08-31 18:39:32 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,8 +15,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i])
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		num <<= 1;  /* Left shift by 1 */
 		num += (b[i] - '0');  /* Add the current bit value */
 		i++;
