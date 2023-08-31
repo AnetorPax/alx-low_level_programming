6989222478ead4dd5decc13b613d52626a9df0ca
AnetorPax <anetorpax@gmail.com>
2023-08-31 18:47:44 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int shift = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int found_one = 0;
 
 	if (n == 0)
 	{
@@ -17,14 +18,17 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	while ((1UL << shift) <= n)
-		shift++;
-
-	shift--;
-
-	while (shift >= 0)
+	while (mask > 0)
 	{
-		putchar((n & (1UL << shift)) ? '1' : '0');
-		shift--;
+		if (n & mask)
+		{
+			putchar('1');
+			found_one = 1;
+		}
+		else if (found_one)
+		{
+			putchar('0');
+		}
+		mask >>= 1;
 	}
 }
